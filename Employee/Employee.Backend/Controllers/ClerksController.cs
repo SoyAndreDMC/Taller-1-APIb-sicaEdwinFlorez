681e887cd43c22305b5b4c8da430844dcf35a662
SoyAndreDMC <andresgonzalezsuarez05@gmail.com>
2025-09-13 10:41:27 -0500

    Add Get and Post of Clerks

---
+++ Employee/Employee.Backend/Controllers/ClerksController.cs
@@ -0,0 +1,32 @@
+﻿using Employee.Backend.Data;
+using Employee.Shared.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Employee.Backend.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ClerksController : ControllerBase
+{
+    private readonly DataContext _context;
+
+    public ClerksController(DataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAsync()
+    {
+        return Ok(await _context.Clerks.ToListAsync());
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> PostAsync(Clerk clerk)
+    {
+        _context.Clerks.Add(clerk);
+        await _context.SaveChangesAsync();
+        return Ok(clerk);
+    }
+}
