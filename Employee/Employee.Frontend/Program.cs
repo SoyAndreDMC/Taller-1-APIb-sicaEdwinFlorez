e65e525f75763fc10dd2c5e59b6a315fc7f1e741
SoyAndreDMC <andresgonzalezsuarez05@gmail.com>
2025-11-23 14:53:00 -0500

    Add Auth Links

--- Employee/Employee.Frontend/Program.cs
+++ Employee/Employee.Frontend/Program.cs
@@ -1,6 +1,7 @@
 using Employee.Frontend.AuthenticationProviders;
 using Employee.Frontend.Components;
 using Employee.Frontend.Repositories;
+using Employee.Frontend.Services;
 using Microsoft.AspNetCore.Components.Authorization;
 using MudBlazor.Services;
 
@@ -10,9 +11,12 @@ builder.Services.AddMudServices();
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 builder.Services.AddSingleton(_ => new HttpClient { BaseAddress = new Uri("https://localhost:7028") });
-builder.Services.AddAuthorizationCore();
-builder.Services.AddScoped<AuthenticationStateProvider, AuthenticationProviderTest>();
 builder.Services.AddScoped<IRepository, Repository>();
+builder.Services.AddAuthorizationCore();
+
+builder.Services.AddScoped<AuthenticationProviderJWT>();
+builder.Services.AddScoped<AuthenticationStateProvider, AuthenticationProviderJWT>(x => x.GetRequiredService<AuthenticationProviderJWT>());
+builder.Services.AddScoped<ILoginService, AuthenticationProviderJWT>(x => x.GetRequiredService<AuthenticationProviderJWT>());
 
 var app = builder.Build();
 
